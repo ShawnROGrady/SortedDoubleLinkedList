8e2bb3e20de5c00dfe467caadee27427594bb117
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-10 14:35:14 -0600

    Added support for inserting value via form
    Added functionality to allow the user to insert a value to the list via a form on the webpage. The form to add a value only appears after the user chooses "1." from the initial form.

--- scripts/main.js
+++ scripts/main.js
@@ -1,73 +1,91 @@
 /*
+  Code by Shawn O'Grady
   This file is responsible for taking the user input
+  +The goal is to allow the user to perform all the following through forms on a webpage:
+    1. insert a value
+    2. remove a value
+    3. print the entire list (either forwards or backwards)
+    4. search the list for a specific value
+    5. close the program
+  +Currently I can perform the following via webpage forms:
+    -have the user chose which function to perform
+    -insert a value to the list
+  +The following are still done via alert() pop ups
+    -remove a value
+    -print the List
+    -searfh the list
 */
 //using strict mode for safety
 "use strict";
 import {sortedDLL} from "./sortedDLL.js";
 
 
-//main function:
+var list= sortedDLL();  //the sorted doubly linked list
 
 var startButton = document.getElementById('start');
 var choiceButton=document.getElementById('enter');
-//choiceButton.style.visibility="none";
+var insertButton=document.getElementById("insertEnter");
 
-//var number=document.querySelector('form');
-//number.style.visibility="none";
 
 startButton.onclick = function() {
   startButton.style.visibility="hidden";
   document.getElementById("frm1").style.visibility="visible";
 }
 
-choiceButton.onclick=function(){
+choiceButton.onclick=function main(){
+  var userInput;
   var x=document.getElementById("frm1");
-  var choice=x.elements[0].value
+  var choice=x.elements[0].value;
   alert(choice);
+  document.getElementById("frm1").style.visibility="hidden";
+
+  var userInput;
+  if(choice==1){
+    //insert value
+    addHandler();
+  }
+  else if(choice==2){
+    //remove a value
+    userInput=prompt("enter a value to remove from the list");
+    list.remove(Number(userInput));
+  }
+  else if(choice==3){
+    //print list
+    var dir=prompt("which direction do you wish to print (\"forward\" or \"reverse\")?");
+    list.changePrint(dir);
+    list.print();
+  }
+  else if(choice==4){
+    //search the list
+    userInput=prompt("enter a value to search for");
+    if(list.search(Number(userInput)).found){
+      //value was in list
+      alert(userInput+" is in the list");
+    }else{
+      //not in list
+      alert(userInput+" is not in the list");
+    }
+  }
+  else if(choice==5||choice==null){
+    //close program
+    alert("thank you for using this program");
+  }
+  else{
+    //invalid choice
+    alert("please enter a valid choice");
+
+  }
 }
 
-function main(){
-  var list= sortedDLL();
-  var choice=document.getElementById('form');
-  alert(choice);
-  var userInput;
-  do{
-    //choice=prompt("What would you like to do? \r 1. insert a value to the list \r 2. remove a value from the list \r 3. print the list \r 4. search the list \r 5. terminate program");
-    if(choice==1){
-      //insert value
-      userInput=prompt("enter a value to add to the list");
-      list.insert(Number(userInput));
-    }
-    else if(choice==2){
-      //remove a value
-      userInput=prompt("enter a value to remove from the list");
-      list.remove(Number(userInput));
-    }
-    else if(choice==3){
-      //print list
-      var dir=prompt("which direction do you wish to print (\"forward\" or \"reverse\")?");
-      list.changePrint(dir);
-      list.print();
-    }
-    else if(choice==4){
-      //search the list
-      userInput=prompt("enter a value to search for");
-      if(list.search(Number(userInput)).found){
-        //value was in list
-        alert(userInput+" is in the list");
-      }else{
-        //not in list
-        alert(userInput+" is not in the list");
-      }
-    }
-    else if(choice==5||choice==null){
-      //close program
-      alert("thank you for using this program");
-    }
-    else{
-      //invalid choice
-      alert("please enter a valid choice");
+function addHandler(){
+  alert("hello");
+  document.getElementById("addNode").style.visibility="visible";
+}
 
-    }
-  }while(choice!=5 && choice!=null);
-};
+insertButton.onclick=function(){
+  var insert=document.getElementById('addNode');
+  var userInput=insert.elements[0].value;
+  list.insert(Number(userInput));
+  document.getElementById("addNode").style.visibility="none";  //hide add form
+  document.getElementById("frm1").style.visibility="visible"; //make main form visible again
+}
